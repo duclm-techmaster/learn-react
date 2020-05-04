94b5956d4ddb398dacef11da6e68fd92193c7267
Luong Minh Duc <duclm.techmaster@gmail.com>
2020-05-04 17:07:27 +0700

    Add bài giải Class Component

---
+++ 4-class-component/delete-row/App.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+class Row extends React.Component {
+  render() {
+    const { index, student, deleteRow } = this.props
+    return (
+      <tr data-index={index}>
+        <td>{student.name}</td>
+        <td>
+          <button onClick={e => deleteRow(e, index)}>Delete</button>
+        </td>
+      </tr>
+    );
+  }
+}
+
+class App extends React.Component {
+  deleteRow(e, index) {
+    e.target.closest(`[data-index="${index}"]`).remove()
+  };
+
+  render() {
+    const students = [
+      { name: "Student1" },
+      { name: "Student2" },
+      { name: "Student3" }
+    ];
+
+    return (
+      <table>
+        <tbody>
+          {students.map((student, index) =>
+            <Row
+              key={index} //key không truyền xuống component con bằng props được
+              index={index} //muốn truyền phải sử dụng 1 biến khác
+              student={student}
+              deleteRow={this.deleteRow}
+            />
+          )}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default App;
---
+++ 4-class-component/on-off-button/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+class App extends React.Component {
+  toggle(e) {
+    let status = e.target.textContent;
+    switch (status) {
+      case 'ON':
+        e.target.textContent = 'OFF'
+        break
+      case 'OFF':
+        e.target.textContent = 'ON'
+        break
+      default:
+        return
+    }
+  }
+
+  render() {
+    return <button onClick={this.toggle}>ON</button>
+  }
+}
+
+export default App;
